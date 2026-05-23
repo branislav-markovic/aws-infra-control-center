74bda13ab4f452c5b3b57f44728637f2649f481d
Branislav Markovic <markovic@classic-trader.com>
2026-05-23 18:05:40 +0200

    Separate aws menu

---
+++ aws-menu.ts
@@ -0,0 +1,54 @@
+import type { MenuItem } from "./interfaces/menu-item.js";
+import { styleText } from 'node:util';
+import { EC2Service } from "./services/ec2-service.js";
+import { S3Service } from "./services/s3-service.js";
+import { Interface } from "node:readline/promises";
+
+export function createAwsMenu(
+    ec2Service: EC2Service,
+    s3Service:S3Service,
+    rl: Interface
+): MenuItem[] {
+        const menu: MenuItem[] = [
+        {
+            id: 0,
+            label: 'Exit',
+            icon: '❌',
+            action: async () => {},
+        },
+        {
+            id: 1,
+            label: 'Launch new EC2 Instance',
+            icon: '🚀',
+            action: async () => {
+                let result = await ec2Service.launchInstance();
+                console.log(styleText('green', `EC2 Instance launched with ID: ${result}`));
+            },
+        },
+        {
+            id: 2,
+            label: 'List all EC2 Instances',
+            icon: '📋',
+            action: async () => {
+                let result = await ec2Service.listInstances();
+                console.log(styleText('green', `EC2 Instances:\n${result}`));
+            },
+        },
+        {
+            id: 3,
+            label: 'Create new S3 Bucket',
+            icon: '📦',
+            action: async () => {
+                const bucketName = (await rl.question('Enter the name of the new S3 bucket: ')).trim();
+                if (!bucketName) {
+                    console.log(styleText('red', 'Bucket name cannot be empty. Action cancelled.'));
+                    return;
+                }
+                let result = await s3Service.createNewBucket(bucketName);
+                console.log(styleText('green', 'New S3 Bucket created successfully: ' + result));
+            },
+        },
+    ];
+
+    return menu;
+}
--- index.ts
+++ index.ts
@@ -6,49 +6,11 @@ import { EC2Service } from './services/ec2-service.js';
 import { EC2ServiceFactory } from './factories/ec2-service.factory.js';
 import { S3Service } from './services/s3-service.js';
 import { S3ServiceFactory } from './factories/s3-service.factory.js';
+import { createAwsMenu } from './aws-menu.js';
 
 async function main(ec2Service: EC2Service, s3Service: S3Service) {
     const rl = readline.createInterface({input, output});
-    const menu: MenuItem[] = [
-        {
-            id: 0,
-            label: 'Exit',
-            icon: '❌',
-            action: async () => {},
-        },
-        {
-            id: 1,
-            label: 'Launch new EC2 Instance',
-            icon: '🚀',
-            action: async () => {
-                let result = await ec2Service.launchInstance();
-                console.log(styleText('green', `EC2 Instance launched with ID: ${result}`));
-            },
-        },
-        {
-            id: 2,
-            label: 'List all EC2 Instances',
-            icon: '📋',
-            action: async () => {
-                let result = await ec2Service.listInstances();
-                console.log(styleText('green', `EC2 Instances:\n${result}`));
-            },
-        },
-        {
-            id: 3,
-            label: 'Create new S3 Bucket',
-            icon: '📦',
-            action: async () => {
-                const bucketName = (await rl.question('Enter the name of the new S3 bucket: ')).trim();
-                if (!bucketName) {
-                    console.log(styleText('red', 'Bucket name cannot be empty. Action cancelled.'));
-                    return;
-                }
-                let result = await s3Service.createNewBucket(bucketName);
-                console.log(styleText('green', 'New S3 Bucket created successfully: ' + result));
-            },
-        },
-    ];
+    const menu = createAwsMenu(ec2Service, s3Service, rl);
 
     const menuMap = new Map<number, MenuItem>(
         menu.map(item => [item.id, item])
