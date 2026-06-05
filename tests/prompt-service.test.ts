28f172a364c640da98fcf4242a4ac941cb821787
Branislav Markovic <markovic@classic-trader.com>
2026-06-05 20:21:21 +0200

    Add tests for prompt service

---
+++ tests/prompt-service.test.ts
@@ -0,0 +1,17 @@
+import { test } from 'node:test'
+import assert from 'node:assert';
+import { PromptService } from '../services/prompt-service.js';
+import { Interface } from "node:readline/promises";
+
+test('ask trims whitespace from user input', async () => {
+    const fakeRl = {
+        question: async () => '       hello    '
+    } as unknown as Interface;
+    
+    const promptService = new PromptService(fakeRl);
+    const result = await promptService.ask('Prompt: ');
+    assert.strictEqual(
+        result,
+        'hello'
+    );
+});
